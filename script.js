deb067b149a0d84dfdbf50b4084f7bb233f15068
hugo <45979673+Hugongomsi@users.noreply.github.com>
2019-02-20 20:37:34 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+1.)/////////
+let number=[1,2,3,4,5, 100]
+ let sum=1
+ for(let i=2; i<= 100; i+=2){
+   let x=[i]*[i];
+   console.log(`the sum of square of even number from [1] to [100] is: " ${i} is: ${x}`);
+  sum =sum+x
+  }
+   console.log(`now the total is: ${sum}`)
+   2.)/////////
+   let number=[1,2,3,4,5, 100]
+ let sum=1
+ for(let i=1; i<= 100; i+=2){
+   let x=[i]*[i];
+   console.log(`the sum of square of odd number from [1] to [100] is: " ${i} is: ${x}`);
+  sum =sum+x
+  }
+   console.log(`now the total is: ${sum}`)
+   3.)/////
+   let number=[1,2,3,4,5, 100]
+ let sum=1
+ for(let i=1; i<= 100; i++){
+    let x=[i+1];
+   console.log(`the sum of number from [1] to [100] is: " ${i} is: ${x}`);
+  sum =sum+x
+  }
+    console.log(`now the total is: ${sum}`)
